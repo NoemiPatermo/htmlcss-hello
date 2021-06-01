9b9ba7d5946ec77066ab8cf8b6929004b6f6cc5b
noemi86-cpu <noemipatermo@gmail.com>
2021-06-01 15:35:16 +0200

    esercizio

--- js/script.js
+++ js/script.js
@@ -3,13 +3,39 @@
 
 
 
+//il software deve chiedere per 10 volte all’utente di inserire un numero.
+//var xNum = prompt("inserisci un numero");
+//Il programma stampa la somma di tutti i numeri inseriti.
 
 
-/*se il nmr è maggiore di 5 scrive, altrimenti non lo scrive*/
-var random = Math.floor(Math.random() * 10);
+//js-snack-4
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
+//chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+ /*var listaInvitati = [ 'tizio', 'caio', 'filano', 'martino'];
+ var invitato = ['prego, inserisca il suo nome'];
+ var flag = false; 
 
- document.getElementById("random").innerHTML = random;
+ for (var i = 0; i < listaInvitati.length; i++){
+    if(invitato === listaInvitati[i]){
+        flag = true;
+    }
+} 
+//siamo fuori dal for
+if (flag){
+    document.getElementById("login").innerHTML = "Benvenuto! Gatsby la attende!";
+} else {
+    document.getElementById("login").innerHTML = "Mi dispiace, lei non può entrare";
+} */
 
-if (random>5){
-    document.getElementById("random").innerHTML = random;
-}
+/*js-snack-5
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array. */
+var myList = ['', '',];
+for(var i = 1; i <= 6; i++){
+    var num = parseInt(prompt('inserisci numero'));
+    if(num % 2 !==0){
+        myList.push(num);
+    }
+    
+console.log
